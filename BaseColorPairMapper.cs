e1f413ed5caf1525391143aebdf04ea03557f362
JuvvalaRishitha <juvvalarishitha@gmail.com>
2024-07-21 14:00:55 +0530

    Create BaseColorPairMapper.cs

---
+++ BaseColorPairMapper.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace TelCo.ColorCoder
+{
+    public abstract class BaseColorPairMapper
+    {
+        public abstract ColorPair GetColorPairFromNumber(int pairNumber);
+        public abstract int GetNumberFromColorPair(ColorPair pair);
+
+        protected void ValidatePairNumber(int pairNumber)
+        {
+            int maxPairNumber = ColorMapping.MajorColors.Length * ColorMapping.MinorColors.Length;
+            if (pairNumber < 1 || pairNumber > maxPairNumber)
+            {
+                throw new ArgumentOutOfRangeException($"PairNumber: {pairNumber} is outside the allowed range of 1 to {maxPairNumber}");
+            }
+        }
+    }
+}
