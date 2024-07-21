e2798e95a80c742eefb232468258541a9be007ab
JuvvalaRishitha <juvvalarishitha@gmail.com>
2024-07-21 14:32:43 +0530

    Update and rename ColorPairMapperTests.cs to ColorPairMapperTestsBase.cs

--- ColorPairMapperTestsBase.cs
+++ ColorPairMapperTestsBase.cs
@@ -4,20 +4,20 @@ using System.Drawing;
 
 namespace TelCo.ColorCoder
 {
-    public abstract class ColorPairMapperTests
+    public abstract class ColorPairMapperTestsBase
     {
-        protected void VerifyColorPair(ColorPairMapperBase mapper, int pairNumber, Color expectedMajor, Color expectedMinor)
+        protected void VerifyColorPairMapping(ColorPairMapperBase mapper, int pairNumber, Color expectedMajor, Color expectedMinor)
         {
-            ColorPair testPair = mapper.GetColorPairFromNumber(pairNumber);
+            ColorPair testPair = mapper.MapNumberToColorPair(pairNumber);
             Console.WriteLine($"[In] Pair Number: {pairNumber}, [Out] Colors: {testPair}");
             Debug.Assert(testPair.MajorColor == expectedMajor);
             Debug.Assert(testPair.MinorColor == expectedMinor);
         }
 
-        protected void VerifyPairNumber(ColorPairMapperBase mapper, Color majorColor, Color minorColor, int expectedPairNumber)
+        protected void VerifyNumberMapping(ColorPairMapperBase mapper, Color majorColor, Color minorColor, int expectedPairNumber)
         {
             ColorPair testPair = new ColorPair { MajorColor = majorColor, MinorColor = minorColor };
-            int pairNumber = mapper.GetNumberFromColorPair(testPair);
+            int pairNumber = mapper.MapColorPairToNumber(testPair);
             Console.WriteLine($"[In] Colors: {testPair}, [Out] PairNumber: {pairNumber}");
             Debug.Assert(pairNumber == expectedPairNumber);
         }
