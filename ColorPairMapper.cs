61d11a5950042f14a6d8cafa02385b2188a20b0a
JuvvalaRishitha <juvvalarishitha@gmail.com>
2024-07-21 14:06:28 +0530

    Update ColorPairMapper.cs

--- ColorPairMapper.cs
+++ ColorPairMapper.cs
@@ -1,10 +1,10 @@
 namespace TelCo.ColorCoder
 {
-    public class ColorPairMapper : BaseColorPairMapper
+    public class ColorPairMapper : ColorPairMapperBase
     {
-        public override ColorPair GetColorPairFromNumber(int pairNumber)
+        public override ColorPair MapNumberToColorPair(int pairNumber)
         {
-            ValidatePairNumber(pairNumber);
+            EnsureValidPairNumber(pairNumber);
             int minorIndex = (pairNumber - 1) % ColorMapping.MinorColors.Length;
             int majorIndex = (pairNumber - 1) / ColorMapping.MinorColors.Length;
             return new ColorPair 
@@ -14,7 +14,7 @@ namespace TelCo.ColorCoder
             };
         }
 
-        public override int GetNumberFromColorPair(ColorPair pair)
+        public override int MapColorPairToNumber(ColorPair pair)
         {
             int majorIndex = ColorMapping.GetColorIndex(pair.MajorColor, ColorMapping.MajorColors);
             int minorIndex = ColorMapping.GetColorIndex(pair.MinorColor, ColorMapping.MinorColors);
